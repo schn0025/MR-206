e86a599c6b69b04f26469d5325c1304297bd4a75
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-01-24 15:29:27 +0100

    EX3 non fini

--- TP1/maConnexionBD19.sql
+++ TP1/maConnexionBD19.sql
@@ -22,3 +22,13 @@ FROM LOCVEC.vehicule v JOIN
 WHERE kmactl > 90000;
 
 --Exercices 3
+--R1
+
+SELECT e.nom ||' '|| e.prnm AS "Employé",
+       TO_char(datctr, "YYYY") AS "Année Contrat",
+       decode(cdsx,1,"Monsieur",2,"Madame")||' '||cli.nom AS "Client",
+       cli.localite AS "Localité"
+FROM LOCVEC.client cli 
+    JOIN LOCVEC.contrat cont ON cli.cdcli = cont.cdcli,
+    JOIN LOCVEC.employe e ON e.cdEmp = cont.cdEmp
+WHERE upper(localite) not like 'REIMS%' AND e.cdex = 2 and to_char(datectr, "MM") = 11;
