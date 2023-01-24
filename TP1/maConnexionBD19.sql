f5cc4b544a71febbadfe4243d65506d23a7488be
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-01-24 15:10:41 +0100

    fin EX2

--- TP1/maConnexionBD19.sql
+++ TP1/maConnexionBD19.sql
@@ -6,3 +6,19 @@ TP1
 -- Exercices 1
 SELECT cdctg, intctg
 FROM LOCVEC.categorie;
+
+-- Exercices 2
+SELECT nom ||' '|| prnm AS "Client"
+FROM LOCVEC.client
+WHERE upper(localite) LIKE '%REIMS%'
+ORDER BY nom;
+
+SELECT marque AS "Marque",
+       tpmdl  AS "Type Model",
+       imtrcl AS "Immatriculation",
+       kmactl AS "Kilométrage actuel"
+FROM LOCVEC.vehicule v JOIN
+     LOCVEC.modele m ON v.cdmdl = m.cdmdl
+WHERE kmactl > 90000;
+
+--Exercices 3
