dc1e019e13a39c0118d637b5578ccb4df281a234
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-01-31 15:24:39 +0100

    ajout jusqu'a la requet 5 comprise'

--- TP1/maConnexionBD19.sql
+++ TP1/maConnexionBD19.sql
@@ -23,12 +23,98 @@ WHERE kmactl > 90000;
 
 --Exercices 3
 --R1
-
 SELECT e.nom ||' '|| e.prnm AS "Employé",
-       TO_char(datctr, "YYYY") AS "Année Contrat",
-       decode(cdsx,1,"Monsieur",2,"Madame")||' '||cli.nom AS "Client",
+       TO_char(datctr, 'YYYY') AS "Année Contrat",
+       decode(e.cdsx, 1, 'Monsieur',2 ,'Madame') || ' ' || cli.nom AS "Client",
        cli.localite AS "Localité"
 FROM LOCVEC.client cli 
-    JOIN LOCVEC.contrat cont ON cli.cdcli = cont.cdcli,
-    JOIN LOCVEC.employe e ON e.cdEmp = cont.cdEmp
-WHERE upper(localite) not like 'REIMS%' AND e.cdex = 2 and to_char(datectr, "MM") = 11;
+    JOIN LOCVEC.contrat cont ON cli.cdcli = cont.cdcli
+    JOIN LOCVEC.employe e    ON e.cdEmp = cont.cdEmp
+WHERE upper(localite) not like 'REIMS%' AND e.cdsx = 2 and to_char(datctr, 'MM') = '11';
+
+--R2
+SELECT nom ||' '|| prnm AS "Client",
+      marque || '-' || tpmdl AS "Modèle",
+      imtrcl AS "Immatriculation"
+FROM LOCVEC.client cli 
+    JOIN LOCVEC.contrat c  ON c.cdcli = cli.cdcli
+    JOIN LOCVEC.vehicule v ON c.cdvhc = v.cdvhc
+    JOIN LOCVEC.modele m   ON m.cdmdl = v.cdmdl
+WHERE to_char(datctr, 'MM/YYYY') = '09/2018'
+    AND months_between(SYSDATE, datprms) > 120
+ORDER BY 2,1;
+
+--R3
+--a
+SELECT marque|| '-' || tpMdl AS "Modèle",
+       imtrcl AS "Immatriculation",
+       to_char(datPmc, 'YYYY') AS "Année Mise en circulation"
+FROM LOCVEC.vehicule v
+     JOIN LOCVEC.modele m ON m.cdMdl = v.cdMdl
+     JOIN LOCVEC.categorie c ON c.cdCtg = m.cdCtg
+WHERE upper(intCtg) = 'STANDARD'
+ORDER BY 3;
+
+--b
+SELECT marque|| '-' || tpMdl AS "Modèle",
+       imtrcl AS "Immatriculation",
+       to_char(datPmc, 'YYYY') AS "Année Mise en circulation"
+FROM LOCVEC.vehicule v
+     JOIN LOCVEC.modele m ON m.cdMdl = v.cdMdl
+WHERE m.cdCtg IN (SELECT cdCtg
+                 FROM LOCVEC.categorie
+                 WHERE upper(intCtg) = 'STANDARD')
+ORDER BY 3;
+
+--R4
+-- a
+SELECT DISTINCT nom ||' '|| prnm AS "Client",
+       DECODE(SUBSTR(CP,1,2),51,'Marne','Autre')AS "departement"
+FROM LOCVEC.client cli
+     JOIN LOCVEC.contrat c ON c.cdCli = cli.cdCli
+     JOIN LOCVEC.typeContrat tp ON tp.cdtpCtr = c.cdTpCtr
+WHERE months_between(sysdate,datNs)/12 BETWEEN 50 AND 60
+AND upper(intTpCtr) LIKE '%FORFAITAIRE%';
+
+--b
+SELECT DISTINCT nom ||' '|| prnm AS "Client",
+       DECODE(SUBSTR(CP,1,2),51,'Marne','Autre')AS "departement"
+FROM LOCVEC.client cli
+     JOIN LOCVEC.contrat c ON c.cdCli = cli.cdCli
+WHERE months_between(sysdate,datNs)/12 BETWEEN 50 AND 60
+      AND c.cdTpCtr IN ( SELECT cdtpCtr
+                         FROM LOCVEC.typeContrat
+                         WHERE upper(intTpCtr) LIKE '%FORFAITAIRE%');
+                         
+-- R5
+--a
+SELECT DISTINCT imtrcl AS "Immatriculation",
+       months_between(sysdate,datPmc ) AS "Ancienneté",
+       kmActl AS "kilométrage"
+FROM LOCVEC.vehicule v
+     JOIN LOCVEC.modele m  ON m.cdMdl = v.cdMdl 
+     JOIN LOCVEC.Contrat c ON c.cdVhc = v.cdVhc
+WHERE marque IN ('OPEL', 'RENAULT', 'PEUGEOT');
+
+--b
+SELECT DISTINCT imtrcl AS "Immatriculation",
+       months_between(sysdate,datPmc ) AS "Ancienneté",
+       kmActl AS "kilométrage"
+FROM LOCVEC.vehicule v     
+     JOIN LOCVEC.Contrat c ON c.cdVhc = v.cdVhc
+WHERE cdMdl IN (SELECT cdMdl
+                FROM LOCVEC.modele 
+                WHERE marque IN ('OPEL', 'RENAULT', 'PEUGEOT'));
+                
+--R6
+
+
+
+
+
+
+
+
+
+
+
