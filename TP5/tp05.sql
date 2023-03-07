4481422b6afb388b5fbffe7270e7e53de05d246a
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-03-07 15:25:33 +0100

    ajout jusqu'a r43.b

--- TP5/tp05.sql
+++ TP5/tp05.sql
@@ -67,8 +67,34 @@ WHERE NOT EXISTS (SELECT NULL
                   FROM LOCVEC.vehicule vhc
                   WHERE vhc.cdmdl = mdl.cdmdl);
 
+-- R43
+-- a) avec des jointures
+SELECT DISTINCT cli.nom || ' ' || cli.prnm AS "Client",
+       NVL(cli.prfs, 'SANS PROFESSION ') AS "Profession"
+FROM LOCVEC.client cli
+     JOIN LOCVEC.contrat cont ON cli.cdcli = cont.cdcli
+     JOIN LOCVEC.vehicule vhc ON vhc.cdvhc = cont.cdvhc
+     JOIN LOCVEC.modele m ON m.cdmdl = vhc.cdmdl
+     JOIN LOCVEC.categorie cat ON cat.cdctg = m.cdctg
+     JOIN LOCVEC.typeContrat tpCtr ON tpCtr.cdTpCtr = cont.cdTpCtr
+WHERE upper(tpCtr.intTpCtr) = 'STANDARD JOUR + KM'
+      AND upper(cat.intCtg) = 'STANDARD';
+      
+-- b) sous-requêtes avec IN
+SELECT DISTINCT cli.nom || ' ' || cli.prnm AS "Client",
+       NVL(cli.prfs, 'SANS PROFESSION ') AS "Profession"
+FROM LOCVEC.client cli
+WHERE cli.cdcli IN (SELECT cdcli
+                    FROM LOCVEC.contrat
+                    WHERE cdVhc IN (SELECT cdvhc 
+                                    FROM LOCVEC.vehicule
+                                    WHERE cdmdl in (SELECT cdmdl
+                                                    FROM LOCVEC.modele
+                                                    WHERE cdctg IN (SELECT cdctg
+                                                                    FROM LOCVEC.categorie
+                                                                    WHERE upper(intCtg) = 'STANDARD')))
+                    AND cdtpctr IN (SELECT cdtpctr
+                                    FROM LOCVEC.typeContrat
+                                    WHERE upper(intTpCtr) = 'STANDARD JOUR + KM'));
 
-
-
-
-
+-- b) sous-requête avec EXISTS
